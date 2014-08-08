106448825c8be42f9d6be4102f98abc5a1615b08
ENAML <EthanACline@gmail.com>
2014-08-08 08:24:51 -0400

    put all personal JS into JQFile, can remove file that formerly contained all the Snap SVG code

--- public/javascripts/jqFile.js
+++ public/javascripts/jqFile.js
@@ -1,27 +1,78 @@
 $(document).ready(function(){
 
-  // var numberOfClones = 500;
-  // var el = $(".four04");
-  // for (var i = 0; i < numberOfClones; i++){
-  //   var newEl = el.clone();
-  //   $("#background").append(newEl);
-  // }
 
+  /////////////////////////////
+  /////// START SVG CODE
+  /////////////////////////////
+  var colors = ['#00ff00','#ffff00' ,'#2d42ff','#882dff', '#fff', '#ff0000'];
+
+  var colorPicker = function(colors) {
+    return colors[Math.floor(Math.random()*colors.length)];
+  };
+
+  var s = Snap("#svg");
+
+  Snap.load("../images/VM_Splash02.svg", function (f) {
+    var vm404 = f.select('#vm404');
+    var circle1 = f.select('#circle1');
+    var circle2 = f.select('#circle2');
+    var face = f.select('#face');
+    var facePaths = f.selectAll('.facePath');
+
+    circle1.animate({'fill': '#000'},0);
+
+    facePaths.forEach(function(el) {
+      el.animate({fill: colorPicker(colors)},600);
+    });
+
+    //animate face children uniquely
+    // function animate() {
+    //   setTimeout(function() {
+    //       facePaths.forEach(function(el) {
+    //         el.animate({fill: colorPicker(colors)},1000);
+    //       });
+    //       requestAnimationFrame(animate);
+    //   }, 500);
+    // }
+    // requestAnimationFrame(animate);
+
+    var intervals = [];
+
+    // global vars
+    startAnim = function() {
+      facePaths.forEach(function(el) {
+        var time = 500+~~(1000*Math.random());
+        intervals.push(setInterval(function(){
+          el.animate({fill: colorPicker(colors)}, time);
+        }, time - 200));
+      });
+    };
+    stopAnim = function(){
+      intervals.forEach(clearInterval);
+    };
+
+    startAnim();
+
+    s.append( f ); // adds to document
+
+    // SVG LOCAL LOAD ERROR: http://stackoverflow.com/questions/20107279/snap-load-local-svg-fails-to-load
+
+  });
+
+  /////////////////////////////
+  /////// START JQ CODE
+  /////////////////////////////
 
   $('#svg').hover(
     function() {
-      running = false;
+      stopAnim();
       // console.log('in');
       $('#background').show();
       $('.play').show();
       $('#text').hide();
-      // !function loop() {
-      //   $('.four04').animate({backgroundColor: '#fff', color: '#000'},5);
-      //   $('.four04').animate({backgroundColor: '#000', color: '#fff'},5, loop);
-      // }();
 
     }, function() {
-      running = true;
+      startAnim();
       // console.log('out');
       $('#background').hide();
       $('.play').hide();
@@ -31,15 +82,19 @@ $(document).ready(function(){
 
   $('.play').hover(
     function() {
-      running = false;
+      stopAnim();
       $('#background').show();
       $('.play').show();
       $('#text').hide();
     }, function() {
-      running = true;
+      startAnim();
       $('#background').hide();
       $('.play').hide();
       $('#text').show();
     }
   );
 });
+
+
+
+
